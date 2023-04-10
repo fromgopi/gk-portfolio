26d4aaee1825d99785e883283c6f52f64d731492
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-10 20:28:28 +0530

    adding pics in mid and layout section
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
+import Layout from '@/components/Layout'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import profilePic from '../../public/images/profile/dev-pic-2-bn.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 	return (
-		<main className='flex  items-center text-dark w-full min-h-screen'>
-			Home
+		<main className='flex items-center text-dark w-full min-h-screen'>
+			<Layout className='pt-0'>
+				<div className='flex items-center justify-between w-full'>
+					<div className=''>
+						<Image src={profilePic} alt='Oscarmild' className='w-full h-auto' />
+					</div>
+				</div>
+			</Layout>
 		</main>
 	)
 }
