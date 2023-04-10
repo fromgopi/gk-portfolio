dfd46547d97b0ecfbf81de675766f6fc1a082f18
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-10 19:56:32 +0530

    adding some layout for the site
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className='text-black'>Home</main>
-  )
+	return (
+		<main className='flex  items-center text-dark w-full min-h-screen'>
+			Home
+		</main>
+	)
 }
