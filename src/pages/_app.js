7144fbfbc3a0a975e1c77f4a3295da4f4f7efd3c
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-10 19:45:39 +0530

    initial commit
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ src/pages/_app.js
@@ -0,0 +1,20 @@
+import NavBar from '@/components/NavBar'
+import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
+
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <NavBar/>
+      <Component {...pageProps} />
+    </main>
+    </>    
+  )
+}
