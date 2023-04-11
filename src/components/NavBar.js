fe49584374f4df1a65375590b7606a99ae2b370f
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-11 08:00:54 +0530

    making some classname changes in the navbar
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,7 @@ const CustomLink = ({href, title, className=''}) => {
         className={`${className} relative group `}
         >
             {title}
-            <span className={`h-[1px] inline-block bg-red-600 absolute left-0 -bottom-0.5 group-hover: w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`
+            <span className={`h-[1px] inline-block bg-red-600 absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`
         }>&nbsp;</span>
         </Link>
     )
