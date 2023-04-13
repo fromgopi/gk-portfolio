ae1b91a603d4e7d8886dc05e10d945ef7363d8a9
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-13 08:24:24 +0530

    making some classname changes in the navbar
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,19 +1,56 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
-const AnimatedText = ({text, className=''}) => {
-    const span = text.split(" ").map((word, index) => {
-        return (
-            <span key={word+'-'+index}
-                className='inline-block'
-            >
-                {word}&nbsp;
-            </span>
-        )
-    })
+const quote = {
+    initial: {
+        opacity:1,
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            staggerChildren: 0.1
+        }
+    }
+}
+
+const singleWord = {
+    initial: {
+        opacity:0,
+        y:50
+    },
+    animate: {
+        opacity: 1,
+        y:0,
+        transition: {
+            delay: 1
+        }
+    }
+}
+
+const AnimatedText = ({text, className=''}) => {    
     return (
         <div className='w-full mx-auto py-2 items-center 
                 justify-center text-center overflow-hidden'>
-            <h1 className={`w-full text-dark font-bold capitalize text-8xl ${className}`}>{span}</h1>
+            <motion.h1 className={`w-full text-dark 
+            font-bold capitalize text-8xl ${className}`} 
+            variants={quote}
+            initial="initial"
+            animate="animate"
+            >
+                {
+                    text.split(" ").map((word, index) => {
+                        return (
+                            <motion.span key={word+'-'+index}
+                                className='inline-block'
+                                variants={singleWord}
+                            >
+                                {word}&nbsp;
+                            </motion.span>
+                        )
+                    })
+                }
+            </motion.h1>
         </div>
     )
 }
