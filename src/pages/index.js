a3d451d9fbff3259aa4ce1debf9712307b33d241
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-13 11:38:36 +0530

    chaning favicon and changing the title
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import profilePic from '../../public/images/profile/dev-pic-2-bn.png';
 import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
+import { LinkArrow } from '@/components/Icons';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,14 +17,25 @@ export default function Home() {
 					<div className='w-1/2'>
 						<Image src={profilePic} alt='Oscarmild' className='w-full h-auto' />
 					</div>
+					{/* Intro section */}
 					<div className='w-1/2 flex flex-col items-center self-center'>
 						<AnimatedText text="Turning Vision Into Reality With Code And Design."
 							className='!text-5xl !text-left'
 						/>
-						<p>
+						<p className='my-4 text-base font-medium'>
 							As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
 							Explore my latest projects and articles, showcasing my expertise in React.js and web development.
 						</p>
+						{/* Resume section */}
+						<div className='flex items-center self-start mt-2'>
+						<Link href="/dummy.pdf" target={'_blank'}
+							className='flex items-center bg-dark text-light p-2.5 px-6
+							rounded-lg text-lg font-semibold hover:bg-light hover:text-dark 
+							border-2 border-solid border-transparent hover:border-red-700'
+							download={true}
+						>Resume <LinkArrow className={`w-6 ml-1`}/></Link>
+						<Link href="mailto:muktevigk@oscarmild.me" target={'_blank'}>Contact</Link>
+					</div>
 					</div>
 				</div>
 			</Layout>
