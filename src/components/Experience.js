d6d0b02985e85a3ec0d9ce856d1333eaf081e097
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-18 21:57:02 +0530

    adding some hooks
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useInView, useMotionValue, useSpring } from 'framer-motion';
+import React, { useEffect, useRef } from 'react'
+
+const AnimatedNumber = ({value}) => {
+    const ref = useRef(null);
+    const motionValue = useMotionValue(0)
+    const springValue = useSpring(motionValue, {duration: 3000 })
+    const isInView = useInView({ref})
+    useEffect(() => {
+        if(isInView) {
+            motionValue.set(value);
+        }
+    }, [isInView, value, motionValue]);
+
+    return(
+        <span>
+
+        </span>
+    )
+}
 
 const Experience = () => {
     return (
         <>
+        <div className='flex flex-col items-end justify-center'>
+            <span className='inline-block text-7xl font-bold'>
+                2+
+            </span>
+            <h2 className='text-xl font-medium capitalize text-dark/75'>Startup Experience</h2>
+        </div>
         <div className='flex flex-col items-end justify-center'>
             <span className='inline-block text-7xl font-bold'>
                 40+
@@ -11,7 +36,7 @@ const Experience = () => {
         </div>
         <div className='flex flex-col items-end justify-center '>
             <span className='inline-block text-7xl font-bold'>
-                4+
+                5+
             </span>
             <h2 className='text-xl font-medium capitalize text-dark/75'>Years of Experience</h2>
         </div>
