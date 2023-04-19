badc521898683d3ef271bbc03f02a8456b444f28
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-19 17:42:59 +0530

    adding some components
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ src/components/Biography.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Biography = () => {
+    return (
+        <div className='col-span-3 flex flex-col items-start justify-start'>
+            <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
+            <p className='font-medium'>
+            Hi, I am Gopi Krishna. I am the founder and chairman of Agrigrow Tech Solutions. I was born and raised in India, and after completing my high school education, I pursued my passion for technology by obtaining a bachelors degree in Computer Science from Osmania University.
+            </p>
+            <p className='my-1 font-medium'>
+            As an entrepreneur, I have always been driven by a desire to make a positive impact on the world. I founded Agrigrow Tech Solutions with the vision of revolutionising the agricultural industry and dairy industry through the use of cutting-edge technology.
+            </p>
+            <p className='font-medium'>
+            With my knowledge and experience in the field of computer science, I have been able to create innovative solutions that have helped farmers to increase their crop yields and improve the efficiency of their operations. My commitment to excellence and my passion for technology have been the driving forces behind my success as an entrepreneur. I am always looking for new opportunities to grow and expand my business, and I am constantly striving to stay ahead of the curve in this rapidly evolving industry. If you are looking for a talented and innovative leader who can help take your business to the next level, I would love to hear from you.
+            </p>
+            </div>
+    )
+}
+
+export default Biography
