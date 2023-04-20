728009bfba864d1fca010bee418e96a4b34da6a2
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-20 17:50:45 +0530

    adding some components
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,18 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-		fontFamily: {
-			mont: ['var(--font-mont)', ...fontFamily.sans]
-		},
-		colors: {
-			dark: "#1b1b1b",
-			light: "#f5f5f5",
-			primary: "#B63E96", // 240,86,199
-			primaryDark: "#58E6D9", // 80,230,217
-			}    
+			fontFamily: {
+				mont: ['var(--font-mont)', ...fontFamily.sans]
+			},
+			colors: {
+				dark: "#1b1b1b",
+				light: "#f5f5f5",
+				primary: "#B63E96", // 240,86,199
+				primaryDark: "#58E6D9", // 80,230,217
+			},
+			backgroundImage: {
+				circularLight: "repeating-radial-gradient(rgba(0,0,0,0.5), #f5f5f5 5px, #f5f5f5 100px)"
+			}
 		},
 	},
 	plugins: [],
