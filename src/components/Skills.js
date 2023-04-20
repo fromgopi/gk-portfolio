d0f8bab1cae67a65e844d498b7855ec6cf8b061c
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-04-20 20:19:42 +0530

    adding some components
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 
-
+const Skill = ({name, x, y}) => {
+    return(
+        <motion.div className='flex items-center justify-center 
+                rounded-full font-semibold  bg-red-600 text-light py-3 px-6
+                shadow-dark cursor-move absolute'
+                whileHover={{scale:1.03}}
+                initial={{x:0,y:0}}
+                animate={{x:x, y:y}}
+                transition={{duration: 1.5}}
+            >
+                    {name}
+            </motion.div>
+    )
+}
 
 const Skills = () => {
     return (
@@ -10,12 +23,23 @@ const Skills = () => {
         <div className='w-full h-screen relative flex items-center 
             justify-center rounded-full bg-circularLight'>
             
-            <motion.div className='flex items-center justify-center 
+            {/* <motion.div className='flex items-center justify-center 
                 rounded-full font-semibold  bg-dark text-light p-8 shadow-dark cursor-move'
                 whileHover={{scale:1.03}}
             >
                     Web
-            </motion.div>
+            </motion.div> */}
+            <Skill name='Web' x="0vw" y="0vw" />
+            <Skill name='HTML' x="-22vw" y="2vw" />
+            <Skill name='SpringBoot' x="20vw" y="6vw" />
+            <Skill name='React' x="0vw" y="12vw" />
+            <Skill name='Nextjs' x="-20vw" y="-15vw" />
+            <Skill name='Django' x="15vw" y="-12vw" />
+            <Skill name='Apache Airflow' x="32vw" y="-5vw" />
+            <Skill name='Spark' x="0vw" y="-20vw" />
+            <Skill name='Firebase' x="-25vw" y="19vw" />
+            <Skill name='go' x="18vw" y="18vw" />
+            
         </div>
         </>
     )
