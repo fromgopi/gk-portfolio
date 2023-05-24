59a9307e02af9a0a959f9195c91d9ad8e1d7b6b6
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-05-24 19:38:32 +0530

    adding more compoents
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { motion, useScroll } from 'framer-motion'
 import LiIcon from './LiIcon'
 
 const Details = ({position, company, companyLink, time, address, work}) => {
+    const ref = useRef(null);
     return(
-        <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-            <LiIcon/>
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+            <LiIcon reference={ref}/>
             <div>
                 <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink}
                 target='_blank'
--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion';
 
-const LiIcon = () => {
+const LiIcon = ({reference}) => {
+    const ref = useRef(null);
+    const {scrollYProgress} = useScroll(
+        {
+            target: reference,
+            offset: ["center end", "center center"]
+        }
+    );
     return (
         <figure className='absolute left-0 stroke-dark'>
             <svg width="75" height="75" viewBox='0 0 100 100'>
                 <circle cx="75" cy="50" r="20" className='stroke-primary stroke-1 fill-none'/>
-                <circle cx="75" cy="50" r="20" className=' stroke-[4px] fill-light'/>
-                <circle cx="75" cy="50" r="10" className=' stroke-1 fill-primary'/>
+                <motion.circle cx="75" cy="50" r="20" className=' stroke-[4px] fill-light'
+                style={{
+                    pathLength : scrollYProgress
+                }}
+                />
+                <circle cx="75" cy="50" r="10" className='animate-pulse stroke-1 fill-primary'/>
             </svg>
         </figure>
     )
