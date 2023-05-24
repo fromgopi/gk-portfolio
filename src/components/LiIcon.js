411a926395186571dc5be64513b6e5a45775dac6
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-05-24 20:28:10 +0530

    adding more compoents
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -11,7 +11,7 @@ const LiIcon = ({reference}) => {
     );
     return (
         <figure className='absolute left-0 stroke-dark'>
-            <svg width="75" height="75" viewBox='0 0 100 100'>
+            <svg className='-rotate-90' width="75" height="75" viewBox='0 0 100 100'>
                 <circle cx="75" cy="50" r="20" className='stroke-primary stroke-1 fill-none'/>
                 <motion.circle cx="75" cy="50" r="20" className=' stroke-[4px] fill-light'
                 style={{
